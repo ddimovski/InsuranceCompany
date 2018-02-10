a4625b25590baf2203b253136bf1c442ffc64046
Daniel <dimovski.d@outlook.com>
2018-02-10 15:08:55 +0100

    Ask users to log in before they can buy a policy

--- Account/Login.aspx.cs
+++ Account/Login.aspx.cs
@@ -18,7 +18,6 @@ namespace InsuranceCompanyWebApp.Account
             RegisterHyperLink.NavigateUrl = "Register";
             // Enable this once you have account confirmation enabled for password reset functionality
             //ForgotPasswordHyperLink.NavigateUrl = "Forgot";
-            OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
             var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
             if (!String.IsNullOrEmpty(returnUrl))
             {
